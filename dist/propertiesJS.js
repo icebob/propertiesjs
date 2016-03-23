7a4d6ad13612659d2f37adf7664a5d661ed717dc
Icebob <mereg.norbert@gmail.com>
2016-03-23 11:38:52 +0100

    Release v1.6.0

--- dist/propertiesJS.js
+++ dist/propertiesJS.js
@@ -1,12 +1,12 @@
 /**
  * propertiesjs - Javascript properties editor for browsers
- * @version v1.5.1
+ * @version v1.6.0
  * @link https://github.com/icebob/propertiesjs
  * @license MIT
  * Copyright (c) 2016 Icebob
  * 
  * 
- * Build Date: Wed Mar 23 2016 11:29:38 GMT+0100 (Közép-európai téli idő )
+ * Build Date: Wed Mar 23 2016 11:38:17 GMT+0100 (Közép-európai téli idő )
  * 
  */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
